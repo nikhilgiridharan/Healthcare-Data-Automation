12f162e960f1cc16f7733220b7263ab4bfe2958e
Nikhil Giridharan <134903029+nikhilgiridharan@users.noreply.github.com>
2025-01-02 21:48:10 -0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# AWS S3 Configuration
+AWS_ACCESS_KEY = "your-aws-access-key"
+AWS_SECRET_KEY = "your-aws-secret-key"
+S3_BUCKET_NAME = "your-s3-bucket-name"
+S3_FOLDER_PATH = "incoming-data/"
+
+# MySQL Configuration
+MYSQL_USERNAME = "your-mysql-username"
+MYSQL_PASSWORD = "your-mysql-password"
+MYSQL_HOST = "localhost"
+MYSQL_PORT = "3306"
+MYSQL_DB = "healthcare_db"
